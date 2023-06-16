a5d294c7c425c970069a3be7544dff3ec9ea07a6
Christian Jans <cjjans@ualberta.ca>
2023-06-16 09:42:25 -0600

    Use new region points file

--- mesonet/activity_analyzer.py
+++ mesonet/activity_analyzer.py
@@ -11,33 +11,6 @@ from tiff_converter import MultiImageTiff
 REGION_POINTS_WIDTH_MAX = 512
 REGION_POINTS_HEIGHT_MAX = 512
 
-INDEX_TO_LABEL = [38, 22, 37, 25, 28, 33, 36, 39, 21, 27, 35, 29, 26, 24, 40,
-                  23, 32, 34, 30, 31, 2, 3, 18, 12, 7, 4, 15, 1, 19, 13, 20, 5,
-                  11, 14, 17, 16, 0, 8, 6, 10, 9]
-
-# NOTE: The complement region label seems to be 40 minus the given region label.
-# COMPLEMENT_LABELS = {
-#     22: 18,
-#     21: 19,
-#     26: 14,
-#     23: 17,
-#     27: 13,
-#     29: 11,
-#     25: 15,
-#     30: 10,
-#     24: 16,
-#     28: 12,
-#     39: 1,
-# }
-
-
-def _index_to_label(index: int) -> int:
-    return INDEX_TO_LABEL[index]
-
-
-def _label_to_index(label: int) -> int:
-    return INDEX_TO_LABEL.index(label)
-
 
 class MasksManager:
     def __init__(self,
@@ -64,14 +37,6 @@ class MasksManager:
 
         self._populate_masks()
 
-    # @property
-    # def masks(self) -> np.array:
-    #     return self.masks
-
-    # @property
-    # def n_regions(self) -> int:
-    #     return self.n_regions
-
     def _populate_masks(self):
         for point, region in self.region_points.items():
             x_resized, y_resized = self._resize_point(point)
@@ -97,21 +62,21 @@ def fft(args):
     fft_values = [[] for _ in range(masks_manager.n_regions)]
 
     for i, image in enumerate(tiff_images):
+        if i == args.n_frames:
+            break
+
         masked_image = image * masks_manager.masks
         masked_image_average = masked_image.mean(axis=(1, 2))
 
         for j in range(len(fft_values)):
             fft_values[j].append(masked_image_average[j])
 
-        if i > 200:
-            break
-
     for i, values in enumerate(fft_values):
         fft_values_array = np.array(values)
         fft = np.fft.fft(fft_values_array)
         power = fft * np.conjugate(fft)  # Compute power spectrum.
         plt.plot(power)
-        plt.savefig(os.path.join(args.save_dir, f"{i}.png"))
+        plt.savefig(os.path.join(args.save_dir, f"fft_{i}.png"))
         plt.clf()
 
 
@@ -127,30 +92,27 @@ def activity_complements(args):
     data = [[[], []] for _ in range(len(masks_manager.masks) // 2)]
 
     for i, image in enumerate(tiff_images):
+        if i == args.n_frames:
+            break
+
         masked_image = image * masks_manager.masks
         masked_sums = masked_image.sum(axis=(1, 2)) / masks_manager.masks.sum(axis=(1, 2))
 
-        for j in range(len(masks_manager.masks) // 2):
-            region_label = _index_to_label(j)
-            complement_region_label = len(masks_manager.masks) - region_label - 1
-            complement_region_index = _label_to_index(complement_region_label)
-
-            data[j][0].append(masked_sums[j])
-            data[j][1].append(masked_sums[complement_region_index])
-
-        if i > 2000:
-            break
+        for label in range(len(masks_manager.masks) // 2):
+            complement_label = len(masks_manager.masks) - label - 1
+            data[label][0].append(masked_sums[label])
+            data[label][1].append(masked_sums[complement_label])
 
     for i, values in enumerate(data):
-        region_label = _index_to_label(i)
-        complement_region_label = len(masks_manager.masks) - region_label - 1
+        label = i
+        complement_label = len(masks_manager.masks) - label - 1
+        plot_filename = f"complement_{label}-{complement_label}.png"
 
-        plt.title(f"{region_label} - {complement_region_label} activity")
-        plt.plot(values[0], label=region_label)
-        plt.plot(values[1], label=complement_region_label)
+        plt.title(f"{label} - {complement_label} activity")
+        plt.plot(values[0], label=label)
+        plt.plot(values[1], label=complement_label)
         plt.legend()
-        plt.savefig(os.path.join(args.save_dir,
-                                 f"activity_complement_{region_label}-{complement_region_label}.png"))
+        plt.savefig(os.path.join(args.save_dir, plot_filename))
         plt.clf()
 
 
@@ -169,14 +131,14 @@ def activity(args):
     data = [[] for _ in range(len(masks_manager.masks))]
 
     for i, image in enumerate(tiff_images):
+        if i == args.n_frames:
+            break
+
         masked_image = image * masks_manager.masks
         masked_sums = masked_image.sum(axis=(1, 2)) / masks_manager.masks.sum(axis=(1, 2))
 
         for j in range(len(masks_manager.masks)):
             data[j].append(masked_sums[j])
-
-        if i > 36501:
-            break
 
     for i, values in enumerate(data):
         plt.plot(values)
@@ -191,8 +153,9 @@ if __name__ == "__main__":
     parser.add_argument("--image-width", type=int, default=128)
     parser.add_argument("--image-height", type=int, default=128)
     parser.add_argument("--save-dir", type=str, required=True)
+    parser.add_argument("--n-frames", type=int, default=2000)
     args = parser.parse_args()
 
     # fft(args)
-    # activity_complements(args)
-    activity(args)
+    activity_complements(args)
+    # activity(args)
