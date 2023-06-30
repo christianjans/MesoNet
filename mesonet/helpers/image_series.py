4de24cc70771423cbdb364dac44c834a6106e321
Christian Jans <cjjans@ualberta.ca>
2023-06-30 12:32:44 -0600

    Add ability to save specific images from image series

--- mesonet/helpers/image_series.py
+++ mesonet/helpers/image_series.py
@@ -1,3 +1,5 @@
+import argparse
+import os
 from typing import Union
 
 import numpy as np
@@ -136,3 +138,39 @@ class ImageSeriesCreator:
                                   n_frames)
         else:
             raise ValueError(f"Unsupported image filename '{filename}'")
+
+
+def save_images(args):
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    image_series = ImageSeriesCreator.create(args.image_file, 128, 128, "all")
+
+    for image_to_save in args.images_to_save:
+        image_array = image_series.get_frame(image_to_save)
+        image_min, image_max = np.amin(image_array), np.amax(image_array)
+        image_array = np.interp(image_array, [image_min, image_max], [0, 255])
+        image_array = image_array.astype(np.uint8)
+        print(np.max(image_array))
+        print(np.min(image_array))
+        print(image_array.dtype)
+        image_array = np.pad(image_array, (args.padding,), constant_values=0)
+        image = PIL.Image.fromarray(image_array)
+        image.save(os.path.join(args.save_dir, f"{image_to_save}.png"))
+
+
+if __name__ == "__main__":
+    """
+    python mesonet/tiff_converter.py \
+        --image-file ./../matlab/my_data/04_awake_8x8_30hz_36500fr.tif \
+        --images-to-save 0 6000 12000 18000 24000 \
+        --save-dir ./mesonet_inputs/awake_data/atlas_brain
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image-file", type=str, required=True)
+    parser.add_argument("--images-to-save", type=int, nargs="+", required=True)
+    parser.add_argument("--save-dir", type=str, required=True)
+    parser.add_argument("--padding", type=int, default=0)
+    args = parser.parse_args()
+    save_images(args)
