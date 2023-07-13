dda9b9ca6655f2ae1f3cc3cb5874f15b3f3d76d0
Christian Jans <cjjans@ualberta.ca>
2023-07-13 12:10:45 -0600

    Update pipelines

--- mesonet/pipelines.py
+++ mesonet/pipelines.py
@@ -3,29 +3,104 @@ import sys
 
 import mesonet
 
-os.environ["MESONET_GIT"] = "/repo"
+MESONET_PATH = os.getcwd()
+
+
+os.environ["MESONET_GIT"] = MESONET_PATH
 sys.path.insert(0, os.environ["MESONET_GIT"])
 sys.path.insert(0, os.path.join(os.environ["MESONET_GIT"], "mesonet"))
 
 
-input_dir_atlas_brain = "/repo/mesonet_inputs/example_data/pipeline1_2"
-input_dir_brain_atlas = "/repo/mesonet_inputs/example_data/pipeline1_2"
-input_dir_sensory_raw = "/repo/mesonet_inputs/example_data/pipeline3_sensory/sensory_raw"
-input_dir_sensory_maps = "/repo/mesonet_inputs/example_data/pipeline3_sensory/sensory_maps"
-input_dir_mbfm_unet = "/repo/mesonet_inputs/example_data/pipeline4_MBFM-U-Net"
-input_dir_voxelmorph = "/repo/mesonet_inputs/example_data/pipeline5_VoxelMorph"
+# Atlas-to-brain paths
+# --------------------
 
-output_dir_atlas_brain = "/repo/mesonet_outputs/mesonet_outputs_atlas_brain"
-output_dir_brain_atlas = "/repo/mesonet_outputs/mesonet_outputs_brain_atlas"
-output_dir_sensory = "/repo/mesonet_outputs/mesonet_outputs_sensory"
-output_dir_mbfm_unet = "/repo/mesonet_outputs/mesonet_outputs_MBFM_U_Net"
-output_dir_voxelmorph = "/repo/mesonet_outputs/mesonet_outputs_voxelmorph"
+input_dir_atlas_brain = os.path.join(
+        MESONET_PATH, "mesonet_inputs/example_data/pipeline1_2")
+output_dir_atlas_brain = os.path.join(
+        MESONET_PATH, "mesonet_outputs/mesonet_outputs_atlas_brain")
 
-dlc_config = "/repo/mesonet/dlc/atlas-DongshengXiao-2020-08-03/config.yaml"
+input_dir_atlas_brain = os.path.join(
+        MESONET_PATH, "mesonet_inputs/awake1_data/atlas_brain")
+output_dir_atlas_brain = os.path.join(
+        MESONET_PATH, "mesonet_outputs/awake1_atlas_brain")
 
-model_file = "/repo/mesonet/models/DongshengXiao_brain_bundary.hdf5"
-unet_only_model_file = "/repo/mesonet/models/DongshengXiao_unet_motif_based_functional_atlas.hdf5"
-voxelmorph_model_file = "/repo/mesonet/models/voxelmorph/VoxelMorph_Motif_based_functional_map_model_transformed1000.h5"
+input_dir_atlas_brain = os.path.join(
+        MESONET_PATH, "mesonet_inputs/awake1_data/atlas_brain")
+output_dir_atlas_brain = os.path.join(
+        MESONET_PATH, "mesonet_outputs/awake1_atlas_brain_no-unet")
+
+input_dir_atlas_brain = os.path.join(
+        MESONET_PATH, "mesonet_inputs/flash1_data/atlas_brain")
+output_dir_atlas_brain = os.path.join(
+        MESONET_PATH, "mesonet_outputs/flash1_atlas_brain")
+
+# input_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_inputs/awake2_data/atlas_brain")
+# output_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_outputs/awake2_atlas_brain")
+
+# input_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_inputs/awake2_data/atlas_brain")
+# output_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_outputs/awake2_atlas_brain_no-unet")
+
+# input_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_inputs/euthanasia1_data/atlas_brain")
+# output_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_outputs/euthanasia1_atlas_brain")
+
+# input_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_inputs/euthanasia1_data/atlas_brain")
+# output_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_outputs/euthanasia1_atlas_brain_no-unet")
+
+# input_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_inputs/euthanasia2_data/atlas_brain")
+# output_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_outputs/euthanasia2_atlas_brain")
+
+# input_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_inputs/euthanasia2_data/atlas_brain")
+# output_dir_atlas_brain = os.path.join(MESONET_PATH, "mesonet_outputs/euthanasia2_atlas_brain_no-unet")
+
+
+# Brain-to-atlas paths
+# --------------------
+
+input_dir_brain_atlas = os.path.join(
+        MESONET_PATH, "mesonet_inputs/example_data/pipeline1_2")
+output_dir_brain_atlas = os.path.join(
+        MESONET_PATH, "mesonet_outputs/mesonet_outputs_brain_atlas")
+
+
+# Sensory paths
+# -------------
+
+input_dir_sensory_raw = os.path.join(
+        MESONET_PATH, "mesonet_inputs/example_data/pipeline3_sensory/sensory_raw")
+input_dir_sensory_maps = os.path.join(
+        MESONET_PATH, "mesonet_inputs/example_data/pipeline3_sensory/sensory_maps")
+output_dir_sensory = os.path.join(
+        MESONET_PATH, "mesonet_outputs/mesonet_outputs_sensory")
+
+
+# MBFM paths
+# ----------
+
+input_dir_mbfm_unet = os.path.join(
+        MESONET_PATH, "mesonet_inputs/example_data/pipeline4_MBFM-U-Net")
+output_dir_mbfm_unet = os.path.join(
+        MESONET_PATH, "mesonet_outputs/mesonet_outputs_MBFM_U_Net")
+
+
+# VoxelMorph paths
+# ----------------
+
+input_dir_voxelmorph = os.path.join(
+        MESONET_PATH, "mesonet_inputs/example_data/pipeline5_VoxelMorph")
+output_dir_voxelmorph = os.path.join(
+        MESONET_PATH, "mesonet_outputs/mesonet_outputs_voxelmorph")
+
+
+dlc_config = os.path.join(
+        MESONET_PATH, "mesonet/dlc/atlas-DongshengXiao-2020-08-03/config.yaml")
+
+model_file = os.path.join(
+        MESONET_PATH, "mesonet/models/DongshengXiao_brain_bundary.hdf5")
+unet_only_model_file = os.path.join(
+        MESONET_PATH, "mesonet/models/DongshengXiao_unet_motif_based_functional_atlas.hdf5")
+voxelmorph_model_file = os.path.join(
+        MESONET_PATH, "mesonet/models/voxelmorph/VoxelMorph_Motif_based_functional_map_model_transformed1000.h5")
+
 
 config_file_atlas_brain = mesonet.config_project(
     input_dir=input_dir_atlas_brain,
@@ -33,11 +108,11 @@ config_file_atlas_brain = mesonet.config_project(
     mode='test',
     atlas_to_brain_align=True,
     use_voxelmorph=False,
-    use_unet=True,
+    use_unet=False,
     use_dlc=True,
     sensory_match=False,
     mat_save=False,
-    olfactory_check=True,
+    olfactory_check=False,
     config=dlc_config,
     model=model_file,
 )
@@ -52,7 +127,7 @@ config_file_brain_atlas = mesonet.config_project(
     use_dlc=True,
     sensory_match=False,
     mat_save=False,
-    olfactory_check=True,
+    olfactory_check=False,
     config=dlc_config,
     model=model_file,
 )
@@ -103,8 +178,8 @@ config_file_voxelmorph = mesonet.config_project(
 )
 
 # Pipeline 1
-# mesonet.predict_regions(config_file_atlas_brain)
-# mesonet.predict_dlc(config_file_atlas_brain)
+mesonet.predict_regions(config_file_atlas_brain)
+mesonet.predict_dlc(config_file_atlas_brain)
 
 # Pipeline 2
 # mesonet.predict_regions(config_file_brain_atlas)
@@ -118,5 +193,5 @@ config_file_voxelmorph = mesonet.config_project(
 # mesonet.predict_regions(config_file_mbfm_unet)
 
 # Pipeline 5
-mesonet.predict_regions(config_file_voxelmorph)
-mesonet.predict_dlc(config_file_voxelmorph)
+# mesonet.predict_regions(config_file_voxelmorph)
+# mesonet.predict_dlc(config_file_voxelmorph)
