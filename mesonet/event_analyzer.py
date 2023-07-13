d020361ad8c1f9ba1f9726a68d084e32b96ba2a9
Christian Jans <cjjans@ualberta.ca>
2023-07-13 12:11:07 -0600

    Add pupillometry data

--- mesonet/event_analyzer.py
+++ mesonet/event_analyzer.py
@@ -1,8 +1,8 @@
 import os
+from typing import List
 
 import cv2
 import matplotlib.pyplot as plt
-import matplotlib.animation as ani
 import numpy as np
 
 from helpers.image_series import ImageSeriesCreator
@@ -23,116 +23,85 @@ MESOSCALE_START_EVENT_FRAME = 88
 
 PUIPIL_FILENAME = "/Users/christian/Downloads/fc2_save_2023-02-09-135224-0000.avi"
 MESOSCALE_PREPROCESSED_FILENAME = "/Users/christian/Documents/summer2023/matlab/my_data/flash1/02_awake_8x8_30hz_36500fr_FR30Hz_BPF1-5Hz_GSR_DFF0-G4-fr1-36480_FR30Hz_BPF1-5Hz_GSR_DFF0-G4-fr1-36480.raw"
+PUPILLOMETRY_CSV_FILE = "/Users/christian/Documents/summer2023/pupillometry_matlab/example_flash1/fc2_save_2023-02-09-135224-0000Pupil Radii.csv"
 SAVE_DIR = "/Users/christian/Documents/summer2023/MesoNet/data_events/test/"
-# CAPTURE_WINDOW = 0.05  # Amount of time to capture before and after the event.
-CAPTURE_WINDOW = 2  # Amount of time to capture before and after the event.
-PUPIL_START_FRAME = PUPIL_EVENT_FRAMES[0] - MESOSCALE_START_EVENT_FRAME + 1
+
+P_FPS = cv2.VideoCapture(PUIPIL_FILENAME).get(cv2.CAP_PROP_FPS)
+M_FPS = float(os.path.basename(MESOSCALE_PREPROCESSED_FILENAME).split("_")[5][2:-2])
+
+M_F_BEFORE = -15
+M_F_AFTER = 15
+M_F_TOTAL = M_F_AFTER - M_F_BEFORE
 
 
-i = 0
+def time_from_pframe(pframe: int) -> float:
+    return pframe * P_FPS
+
+
+def pframe_to_mframe(pframe: int) -> int:
+    abs_time = time_from_pframe(pframe)
+    m_start_time = time_from_pframe(
+            PUPIL_EVENT_FRAMES[0] - MESOSCALE_START_EVENT_FRAME)
+    frame = round((abs_time - m_start_time) / M_FPS)
+
+    if frame <= 0:
+        raise ValueError(f"Invalid frame number: {frame}")
+    return frame
+
 
 def main():
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
 
-    pupil_video_fps = cv2.VideoCapture(PUIPIL_FILENAME).get(cv2.CAP_PROP_FPS)
-    mesoscale_fps = 30.0  # TODO
     image_series = ImageSeriesCreator.create(MESOSCALE_PREPROCESSED_FILENAME,
                                              128,
                                              128,
                                              "all")
+    pdata: np.ndarray = np.genfromtxt(PUPILLOMETRY_CSV_FILE, delimiter=";")
+    pdata_interval = int(pdata[1][0] - pdata[0][0])
+    pdata = {int(frame): pupil_size for frame, pupil_size in pdata}
 
-    N = 20
-    images = [[] for _ in range(N)]
-    start_time = PUPIL_START_FRAME / pupil_video_fps
-    for pupil_event_frame in PUPIL_EVENT_FRAMES:
-        time = pupil_event_frame / pupil_video_fps
-        time = time + 0.04  # 40 milliseconds.
-        time = time - start_time
-        mesoscale_event_frame = int(time * mesoscale_fps)
-        for i in range(N):
-            images[i].append(image_series.get_frame(mesoscale_event_frame - 1 + i))
+    pupil_sizes: List[List[float]] = []
+    images: List[List[np.ndarray]] = []
+    # images = [[] for _ in range(M_F_TOTAL)]
+    for i, pupil_event_frame in enumerate(PUPIL_EVENT_FRAMES):
+        # Collect pupil size data.
+        pupil_sizes.append([])
+        for j in range(M_F_BEFORE * pdata_interval, M_F_AFTER * pdata_interval):
+            if pupil_event_frame - 1 + j in pdata:
+                pupil_sizes[i].append(pdata[pupil_event_frame - 1 + j])
 
-    average_image = np.mean(np.array(images), axis=0)
+        # Collect mesoscale brain data.
+        images.append([])
+        mframe = pframe_to_mframe(pupil_event_frame)
+        for j in range(M_F_BEFORE, M_F_AFTER):
+            images[i].append(image_series.get_frame(mframe - 1 + j))
+            # images[j].append(image_series.get_frame(mframe - 1 + j))
 
+    # Take the average over the collected data.
+    average_pupil_sizes = np.mean(np.array(pupil_sizes), axis=0)
+    average_images = np.mean(np.array(images), axis=0)
+
+    # Plot the data.
     plt.rcParams.update({'font.size': 5})
-    _, axes = plt.subplots(1, N)
-    for i, image in enumerate(images):
-        average_image = np.mean(np.array(image), axis=0)
-        axes[i].set_title(f"+{(i + 1) / mesoscale_fps:.2f} s")
-        axes[i].axis('off')
-        axes[i].imshow(average_image)
+    pticks = [f"{pdata_interval * i / P_FPS:.3f}"
+              for i in range(M_F_BEFORE, M_F_AFTER)]
+    mticks = [f"{i / M_FPS:.3f}" for i in range(M_F_BEFORE, M_F_AFTER)]
+
+    pfigure = plt.figure(1)
+    paxes = pfigure.gca()
+    paxes.set_ylabel("pupil radius (pixels)")
+    paxes.set_xlabel("time (s)")
+    paxes.set_xticks(range(M_F_TOTAL), pticks)
+    paxes.plot(average_pupil_sizes)
+
+    _, maxes = plt.subplots(1, M_F_TOTAL)
+    for i, average_image in enumerate(average_images):
+        maxes[i].set_title(f"{mticks[i]} s")
+        maxes[i].axis('off')
+        maxes[i].imshow(average_image)
+
     plt.show()
-
-
-    # n_frames = image_series.image_array.shape[0]
-    # n_window_frames = np.floor(CAPTURE_WINDOW * pupil_video_fps)
-
-    # print(image_series.image_array.shape)
-    # print(n_frames)
-
-    # for event_frame in PUPIL_EVENT_FRAMES:
-    #     event_frame_index = event_frame - 1
-    #     earliest_frame_index = int(max(event_frame_index - n_window_frames, 0))
-    #     latest_frame_index = int(min(event_frame_index + n_window_frames, n_frames))
-
-    #     print(earliest_frame_index)
-    #     print(latest_frame_index)
-
-    #     video_array = image_series.image_array[earliest_frame_index:latest_frame_index]
-    #     video_filename = os.path.join(SAVE_DIR, f"event_{event_frame}.avi")
-    #     print(image_series.image_array.dtype)
-
-    #     # figure = plt.figure()
-    #     # global i
-    #     # i = earliest_frame
-    #     # image = plt.imshow(image_series.image_array[0], animated=True)
-    #     # def update(*args):
-    #     #     global i
-    #     #     i += 1
-    #     #     if i >= latest_frame:
-    #     #         return
-    #     #     image.set_array(image_series.image_array[i])
-    #     #     return image
-    #     # animation = ani.FuncAnimation(figure,
-    #     #                               update,
-    #     #                               frames=latest_frame - earliest_frame,
-    #     #                               blit=True)
-    #     # plt.show()
-
-    #     video_writer = cv2.VideoWriter(video_filename,
-    #                                    cv2.VideoWriter_fourcc(*"MJPG"),
-    #                                    pupil_video_fps,
-    #                                    (128, 128),
-    #                                    False)
-
-    #     print(np.max(image_series.image_array[0]))
-    #     print(np.min(image_series.image_array[0]))
-
-    #     for i in range(earliest_frame_index, latest_frame_index):
-    #         frame = image_series.get_frame(i)
-    #         plt.clf()
-    #         plt.imshow(frame)
-    #         plt.title(f"Frame {i}")
-    #         plt.pause(0.03)
-
-    #         # frame = frame.astype(np.float64) / np.max(frame)
-    #         # frame = (2 ** 8 - 1) * frame
-    #         # frame = frame.astype(np.uint8)
-    #         # video_writer.write(frame)
-
-    #         # plt.imshow(frame)
-    #         # plt.show()
-    #         # frame = cv2.normalize(frame, None, 255, 0, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-    #         # frame = cv2.merge([frame, frame, frame])
-    #         # print(frame)
-    #         # cv2.imshow('frame', frame)
-    #         # video_writer.write(frame)
-    #         # plt.imshow(frame)
-    #         # plt.show()
-    #         # cv2.imshow("Window", frame)
-
-    #     video_writer.release()
 
 
 if __name__ == "__main__":
